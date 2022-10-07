aac537987dd63fda4b4cb99eb89f63b4caedaa3e
Amanda Wasinger <amandarm0928@outlook.com>
2022-10-07 14:13:24 -0500

    added word file; added all words and hints

---
+++ words.js
@@ -0,0 +1,38 @@
+const words = [
+    {
+        word: "flower"
+        hint: "Makes fruits."
+    }
+    {
+        word: "leaves"
+        hint: "Trap sunlight to help the plant make food."
+    }
+    {
+        word: "sunlight"
+        hint: ""
+    }
+    {
+        word: "water"
+        hint: "Makes seeds"
+    }
+    {
+        word: "seed"
+        hint: "Makes seeds"
+    }
+    {
+        word: "petals"
+        hint: "Makes seeds"
+    }
+    {
+        word: "roots"
+        hint: "Hold the plant in the ground and collect nutrients from soil."
+    }
+    {
+        word: "stem"
+        hint: "Brings water to all the parts of the plant."
+    }
+    {
+        word: "fruit"
+        hint: "Makes seeds."
+    }
+]
