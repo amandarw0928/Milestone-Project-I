d673a4f71a73fb8ab09afcc8691507e626fd5619
Amanda Wasinger <amandarm0928@outlook.com>
2022-10-09 21:26:09 -0500

    updating hints

--- words.js
+++ words.js
@@ -1,38 +1,38 @@
 let words = [
     {
         word: "flower",
-        hint: "Makes fruits."
+        hint: "This makes a fruit."
     },
     {
         word: "leaves",
-        hint: "Trap sunlight to help the plant make food."
+        hint: "These trap sunlight to help the plant make food."
     },
     {
         word: "sunlight",
-        hint: "Collected by leaves to help make food for the plant."
+        hint: "This is collected by leaves to help make food for the plant."
     },
     {
         word: "water",
-        hint: "Collected by roots to help make food for the plant."
+        hint: "This is collected by roots to help make food for the plant."
     },
     {
         word: "seed",
-        hint: "Makes a new plant."
+        hint: "This can make a new plant."
     },
     {
         word: "petals",
-        hint: "Attract pollinators to the flower."
+        hint: "These attract pollinators to the flower."
     },
     {
         word: "roots",
-        hint: "Hold the plant in the ground and collect nutrients from soil."
+        hint: "These hold the plant in the ground and collect nutrients from soil."
     },
     {
         word: "stem",
-        hint: "Brings water to all the parts of the plant."
+        hint: "This brings water to all the parts of the plant."
     },
     {
         word: "fruit",
-        hint: "Makes seeds."
+        hint: "This makes seeds."
     }
 ]
